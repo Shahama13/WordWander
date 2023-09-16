8662b117b080a6b1bd9e7511c2841335f9dbb921
Shahama13 <128844633+Shahama13@users.noreply.github.com>
2023-09-16 23:49:28 +0530

    Update Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,8 +1,21 @@
 
+import HourglassTopOutlinedIcon from "@mui/icons-material/HourglassTopOutlined";
+import { Typography } from "@mui/material";
+
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",        
+      }}
+    
+    >
+      <HourglassTopOutlinedIcon style={{ marginRight: "0.5rem" }} />
+      <Typography variant="h6">Loading....</Typography>
+    </div>
+  );
+};
 
 export default Loader
